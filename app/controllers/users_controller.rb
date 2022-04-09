2977d6ae85e92a3028d96d975ed5d780c4a0b671
monyatto <atsuuu.0925@gmail.com>
2022-04-09 09:57:55 +0900

    pre follow button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def following
+    @title = Follow.human_attribute_name(:following)
+    @user  = User.find(params[:id])
+    @users = @user.following.order(:id).page(params[:page])
+    render 'follow'
+  end
+
+  def followers
+    @title = Follow.human_attribute_name(:followers)
+    @user  = User.find(params[:id])
+    @users = @user.followers.order(:id).page(params[:page])
+    render 'follow'
+  end
 end
