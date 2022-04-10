74b9a26594456a046679afb61940c40283b952ea
monyatto <atsuuu.0925@gmail.com>
2022-04-11 07:23:38 +0900

    complete rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,22 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
 
   has_many :microposts, dependent: :destroy
-  has_many :active_follows,  class_name:  "Follow",
-                                   foreign_key: "follower_id",
-                                   dependent:   :destroy
-  has_many :passive_follows, class_name:  "Follow",
-                                   foreign_key: "followed_id",
-                                   dependent:   :destroy
+  has_many :active_follows,  class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy, inverse_of: :follower
+  has_many :passive_follows, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :followed
   has_many :following, through: :active_follows,  source: :followed
   has_many :followers, through: :passive_follows, source: :follower
 
-  # ユーザーをフォローする
   def follow(other_user)
-    following << other_user
+    active_follows.create(followed_id: other_user)
   end
 
-  # ユーザーをフォロー解除する
   def unfollow(other_user)
-    active_follows.find_by(followed_id: other_user.id).destroy
+    active_follows.find_by(followed_id: other_user).destroy
   end
 
   # 現在のユーザーがフォローしてたらtrueを返す
