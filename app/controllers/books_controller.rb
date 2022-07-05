34ef98a33da6b50ac3d3a9d4abc7d54869b5dbc1
monyatto <atsuuu.0925@gmail.com>
2022-07-05 18:26:46 +0900

    コメント機能完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,9 @@ class BooksController < ApplicationController
 
   # GET /books/1
   # GET /books/1.json
-  def show; end
+  def show
+    @comment = Comment.new
+  end
 
   # GET /books/new
   def new
@@ -22,50 +24,35 @@ class BooksController < ApplicationController
   def edit; end
 
   # POST /books
-  # POST /books.json
   def create
     @book = Book.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: t('controllers.common.notice_create', name: Book.model_name.human) }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.save
+      redirect_to @book, notice: t('controllers.common.notice_create', name: Book.model_name.human)
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: t('controllers.common.notice_update', name: Book.model_name.human) }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book&.update(book_params)
+      redirect_to @book, notice: t('controllers.common.notice_update', name: Book.model_name.human)
+    else
+      render :edit
     end
   end
 
   # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
-    @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human) }
-      format.json { head :no_content }
-    end
+    @book&.destroy
+    redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human)
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_book
-    @book = Book.find(params[:id])
+    @book = Book.find(params[:id]) if Book.exists?(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[edit update destroy]
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      redirect_to @commentable, notice: t('errors.messages.empty', name: Comment.model_name.human)
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @comment&.update(comment_params)
+      redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment&.destroy
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id]) if Comment.exists?(params[:id])
+  end
+end
